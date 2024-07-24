db22213f658f2a9401084611ac10ce201de4994e
mamtabattersea24 <dev.battersea@gmail.com>
2024-07-24 18:33:11 +0530

    chal rha hai

--- 01_business-name-generator.php
+++ 01_business-name-generator.php
@@ -1,40 +1,4 @@
-<!-- source refernces
-https://namy.ai/
-https://github.com/designbycode/business-name-generator
-
-https://looka.com/business-name-generator/
-https://github.com/fzaninotto/Faker
-https://logo.com/business-name-generator
-https://www.shopify.com/in/tools/business-name-generator
-https://stackoverflow.com/questions/28688442/how-to-generate-random-name-of-person-using-php
-
-https://www.google.com/search?q=thesaurus+api+free&rlz=1C1RXQR_enIN1026IN1026&oq=thesaurus+api+f&gs_lcrp=EgZjaHJvbWUqBwgAEAAYgAQyBwgAEAAYgAQyBggBEEUYOTIKCAIQABiABBiiBKgCCLACAQ&sourceid=chrome&ie=UTF-8
-
-https://www.google.com/search?q=github+repo+for+synonoms+in+php&rlz=1C1RXQR_enIN1026IN1026&oq=github+repo+for+synonoms+in+php&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEJMjk3ODNqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8
-
-https://www.thesaurus.com/browse/repository
--->
-
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Business Name Generator</title>
-</head>
-
-<body>
-  <h1>Business Name Generator</h1>
-  <form method="post">
-    <label for="keywords">Enter Keywords:</label>
-    <input type="text" id="keywords" name="keywords" required>
-
-    <label for="number">Number of Names to Generate:</label>
-    <input type="number" id="number" name="number" min="1" max="100" required>
-
-    <button type="submit">Generate Names</button>
-  </form>
-
-  <?php
+<?php
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $keywords = htmlspecialchars($_POST['keywords']);
@@ -79,9 +43,6 @@ https://www.thesaurus.com/browse/repository
       }
     }
 
-    // Capitalize names
-   
-
     // Ensure all names are unique and limit the number of names to generate
     $generatedNames = array_unique($generatedNames);
     $generatedNames = array_slice($generatedNames, 0, $limit);
@@ -97,7 +58,51 @@ https://www.thesaurus.com/browse/repository
   }
 
 
-  ?>
-</body>
+?>
 
-</html>
+
+<?php include "header.php"; ?>
+
+<div class="container mt-5">
+  <div class="all-page">
+
+  <!-- tools container  Start-->
+    <div class="noome-tool-box">
+
+      <h1>Business Name Generator</h1>
+      <form method="post" class="mt-5">
+        <label for="keywords">Enter Keywords:</label>
+        <input type="text" id="keywords" name="keywords" required>
+
+        <label for="number">Number of Names to Generate:</label>
+        <input type="number" id="number" name="number" min="1" max="100" required>
+
+        <button type="submit" class="way-btn">Generate Names</button>
+      </form>
+
+    </div>
+  <!-- tool container End  -->
+
+
+  </div>
+</div>
+
+<?php include "footer.php"; ?>
+
+<!-- source refernces
+https://namy.ai/
+https://github.com/designbycode/business-name-generator
+https://www.dreamhost.com/tools/business-name-generator/
+
+https://looka.com/business-name-generator/
+https://github.com/fzaninotto/Faker
+https://logo.com/business-name-generator
+https://www.shopify.com/in/tools/business-name-generator
+https://stackoverflow.com/questions/28688442/how-to-generate-random-name-of-person-using-php
+
+https://www.google.com/search?q=thesaurus+api+free&rlz=1C1RXQR_enIN1026IN1026&oq=thesaurus+api+f&gs_lcrp=EgZjaHJvbWUqBwgAEAAYgAQyBwgAEAAYgAQyBggBEEUYOTIKCAIQABiABBiiBKgCCLACAQ&sourceid=chrome&ie=UTF-8
+
+https://www.google.com/search?q=github+repo+for+synonoms+in+php&rlz=1C1RXQR_enIN1026IN1026&oq=github+repo+for+synonoms+in+php&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEJMjk3ODNqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8
+
+https://www.thesaurus.com/browse/repository
+-->
